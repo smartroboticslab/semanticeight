98406e2e0f451240a3c9f398a46cd24d650e3099
Sotiris Papatheodorou <sotirisp@protonmail.com>
2020-05-22 12:33:26 +0300

    Add Newer College dataset reader

--- se_apps/src/reader.cpp
+++ se_apps/src/reader.cpp
@@ -11,6 +11,7 @@
 #include "filesystem.hpp"
 #include "se/str_utils.hpp"
 #include "reader_iclnuim.hpp"
+#include "reader_newercollege.hpp"
 #include "reader_openni.hpp"
 #include "reader_raw.hpp"
 
@@ -42,6 +43,11 @@ se::Reader* se::create_reader(const Configuration& config) {
   } else if (config.sequence_type == "raw"
         && stdfs::path(reader_config.sequence_path).extension() == ".raw") {
     reader = new se::RAWReader(reader_config);
+
+  // NewerCollege reader
+  } else if (config.sequence_type == "newercollege"
+        && stdfs::is_directory(reader_config.sequence_path)) {
+    reader = new se::NewerCollegeReader(reader_config);
 
   } else {
     std::cerr << "Error: Unrecognised file format, file not loaded\n";
