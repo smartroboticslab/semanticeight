7f375c90785cdc119429fb49cd241a16c75e4550
Unknown <funkn@student.ethz.ch>
2020-07-29 09:22:46 +0100

    Add example benchmark configs for cow and lady and icl nuim

--- se_tools/benchmark_scripts/benchmark.py
+++ se_tools/benchmark_scripts/benchmark.py
@@ -8,3 +8,4 @@ from datasets import *
 FULL_BENCHMARK = BenchmarkBase("full-benchmark")
 FULL_BENCHMARK.setup_from_yaml("./config/benchmark/benchmark_base_config.yaml")
 FULL_BENCHMARK.add_datasets([EXAMPLE_DATASET_1, EXAMPLE_DATASET_2])
+# FULL_BENCHMARK.add_datasets([ICL_NUIM, COW_AND_LADY])
--- se_tools/benchmark_scripts/datasets.py
+++ se_tools/benchmark_scripts/datasets.py
@@ -5,7 +5,13 @@
 from _dataset import *
 
 EXAMPLE_DATASET_1 = Dataset("example_dataset_1")
-EXAMPLE_DATASET_1.setup_from_yaml("./config/datasets/example_dataset_1_dataset_config.yaml")
+EXAMPLE_DATASET_1.setup_from_yaml("./config/datasets/example_dataset_1_config.yaml")
 
 EXAMPLE_DATASET_2 = Dataset("example_dataset_2")
-EXAMPLE_DATASET_2.setup_from_yaml("./config/datasets/example_dataset_2_dataset_config.yaml")
+EXAMPLE_DATASET_2.setup_from_yaml("./config/datasets/example_dataset_2_config.yaml")
+
+# ICL_NUIM   = Dataset("icl_nuim")
+# ICL_NUIM.setup_from_yaml("./config/datasets/icl_nuim_dataset_config.yaml")
+#
+# COW_AND_LADY   = Dataset("cow_and_lady")
+# COW_AND_LADY.setup_from_yaml("./config/datasets/cow_and_lady_dataset_config.yaml")
