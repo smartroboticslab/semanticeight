303ae333a671395782fbb8c265ae3ef064632895
Sotiris Papatheodorou <sotirisp@protonmail.com>
2020-10-04 16:05:06 +0100

    Move se::Volume into separate header

--- se_core/include/se/octree_iterator.hpp
+++ se_core/include/se/octree_iterator.hpp
@@ -6,52 +6,15 @@
 #define __OCTREE_ITERATOR_HPP
 
 #include <iterator>
-#include <ostream>
 #include <stack>
 
 #include "node.hpp"
+#include "volume.hpp"
 
 namespace se {
 
   // Forward declare class
   template <typename T> class Octree;
-
-  /*! \brief A cube with data.
-   * Used to represent the data stored in an se::Octree at mutliple scales and
-   * locations.
-   */
-  template <typename T>
-  struct Volume {
-    typedef typename T::VoxelData VoxelData;
-
-    /** The coordinates of the Volume's centre in the Map frame. */
-    Eigen::Vector3f centre_M;
-    /** The lengh of the Volume's edges in metres. */
-    float dim;
-    /** The lengh of the Volume's edges in voxels. */
-    int size;
-    /** The data contained in the Volume. */
-    VoxelData data;
-
-    Volume();
-
-    Volume(const Eigen::Vector3f& centre_M,
-           float                  dim,
-           int                    size,
-           const VoxelData&       data);
-
-    Volume(const Volume& other);
-
-    ~Volume() = default;
-
-    Volume& operator=(const Volume& other);
-
-    bool operator==(const Volume& other) const;
-
-    bool operator!=(const Volume& other) const;
-  };
-
-
 
 
 
