c3fd1ffbe03feab6e1901321bddec4ec475fbe4b
Sotiris Papatheodorou <sotirisp@protonmail.com>
2021-03-09 20:21:37 +0200

    [PORT] Add function to visualize VoxelBlock data using Graphviz

--- se_core/include/se/io/graphviz_io.hpp
+++ se_core/include/se/io/graphviz_io.hpp
@@ -34,6 +34,25 @@ namespace se {
   int to_graphviz(const se::Octree<VoxelT>& octree,
                   const std::string&        filename,
                   const FunctionT           data_to_str);
+
+  /** Generate a Graphviz diagram in filename showing the data the node.
+   * All allocated scales are shown for VoxelBlocks.
+   * A lambda function data_to_str can be used to specify how Node data
+   * is converted to an std::string for visualization similarly to se::to_graphviz.
+   *
+   * \todo Implement Node visualization.
+   * \todo Implement visualization for other kinds of VoxelBlocks.
+   *
+   * \param[in] node        The node to visualize.
+   * \param[in] filename    The filename of the resulting Graphviz file. Graphviz
+   *                        files have a .gv extension by convention.
+   * \param[in] data_to_str The function used to format VoxelT::VoxelData to std::string.
+   * \return 0 on success, non-zero on IO error.
+   */
+  template<typename VoxelT, typename FunctionT>
+  int to_graphviz(const se::Node<VoxelT>& node,
+                  const std::string&      filename,
+                  const FunctionT         data_to_str);
 } // namespace se
 
 #include "graphviz_io_impl.hpp"
--- se_core/include/se/io/graphviz_io_impl.hpp
+++ se_core/include/se/io/graphviz_io_impl.hpp
@@ -6,6 +6,23 @@
 #define __GRAPHVIZ_IO_IMPL_HPP
 
 namespace se {
+  std::string _scale_to_graphviz_colour(const int scale) {
+    switch (scale) {
+      case 0:
+        return std::string("aquamarine");
+      case 1:
+        return std::string("coral");
+      case 2:
+        return std::string("lightblue");
+      case 3:
+        return std::string("hotpink");
+      default:
+        return std::string("");
+    }
+  }
+
+
+
   template<typename VoxelT, typename FunctionT>
   int to_graphviz(const se::Octree<VoxelT>& octree,
                   const std::string&        filename,
@@ -82,6 +99,80 @@ namespace se {
       for (const auto& data_str : child_data) {
         fprintf(f, "\t%s\n", data_str.c_str());
       }
+    }
+
+    // Write the footer and close the file
+    fprintf(f, "}\n");
+    return fclose(f);
+  }
+
+
+
+  template<typename VoxelT, typename FunctionT>
+  int to_graphviz(const se::Node<VoxelT>& node,
+                  const std::string&      filename,
+                  const FunctionT         data_to_str) {
+    using BlockT = VoxelBlockSingleMax<VoxelT>;
+    // Open the file and write the Graphviz header
+    FILE* f = fopen(filename.c_str(), "w");
+    if (!f) {
+      return 1;
+    }
+    // Set most of the graph style options
+    fprintf(f, "strict digraph octree_node {\n");
+    fprintf(f, "\tnode [shape=box, style=\"filled\", fontname=\"Monospace\", fontsize=12];\n");
+    fprintf(f, "\tedge [arrowhead=none];\n");
+
+    if (node.isBlock()) {
+      // TODO SEM This will throw an std::bad_cast for non-MultiresOFusion
+      const BlockT& block = dynamic_cast<const BlockT&>(node);
+      const int current_scale = block.current_scale();
+      // Visualize all scales
+      for (int scale = current_scale; scale <= BlockT::max_scale; scale++) {
+        const int scale_size = BlockT::scaleSize(scale);
+        // Iterate over all voxels at this scale
+        for (int z = 0; z < scale_size; z++) {
+          for (int y = 0; y < scale_size; y++) {
+            for (int x = 0; x < scale_size; x++) {
+              // Generate the voxel label
+              const int voxel_idx = x + y * scale_size + z * scale_size * scale_size;
+              const int voxel_size = BlockT::scaleVoxelSize(scale);
+              const auto& data = block.data(voxel_idx, scale);
+              const std::string coord_str ("V "
+                  + std::to_string(x) + " "
+                  + std::to_string(y) + " "
+                  + std::to_string(z) + " ("
+                  + std::to_string(voxel_idx) + ")  S:"
+                  + std::to_string(voxel_size) + "\\n");
+              const std::string label = coord_str + data_to_str(data);
+              // Colour depending on the scale
+              const std::string colour = _scale_to_graphviz_colour(scale);
+              // Write the voxel info
+              const std::string id = std::to_string(scale) + "_" + std::to_string(voxel_idx);
+              fprintf(f, "\tV%s [fillcolor=%s,label=\"%s\"];\n",
+                  id.c_str(), colour.c_str(), label.c_str());
+              // Connect this voxel to the voxels at lower scales
+              if (scale > current_scale) {
+                const int child_scale_size = BlockT::scaleSize(scale - 1);
+                fprintf(f, "\tV%s -> {", id.c_str());
+                for (int zz = 0; zz < 2; zz++) {
+                  for (int yy = 0; yy < 2; yy++) {
+                    for (int xx = 0; xx < 2; xx++) {
+                      const int child_idx = (2 * x + xx) + (2 * y + yy) * child_scale_size + (2 * z + zz) * child_scale_size * child_scale_size;
+                      const std::string child_id = std::to_string(scale - 1) + "_" + std::to_string(child_idx);
+                      fprintf(f, " V%s", child_id.c_str());
+                    }
+                  }
+                }
+                fprintf(f, "}\n");
+              }
+            }
+          }
+        }
+      }
+    } else {
+      // TODO SEM implement for nodes
+      throw std::runtime_error("se::to_graphviz() not implemented for Nodes");
     }
 
     // Write the footer and close the file
