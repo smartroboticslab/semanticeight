ecd18963415bf74301643eaee73cc50175372d69
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2021-11-23 19:50:59 +0000

    Add pose history grid

---
+++ se_denseslam/include/se/pose_grid_history.hpp
@@ -0,0 +1,87 @@
+// SPDX-FileCopyrightText: 2021 Smart Robotics Lab, Imperial College London
+// SPDX-FileCopyrightText: 2021 Sotiris Papatheodorou
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef POSE_GRID_HISTORY_HPP
+#define POSE_GRID_HISTORY_HPP
+
+#include <Eigen/Dense>
+#include <Eigen/StdVector>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include "se/utils/math_utils.h"
+
+namespace se {
+
+/** A dense grid storing the number of poses (x,y,z,yaw) encountered in each cell.
+ */
+class PoseGridHistory {
+    public:
+    typedef uint8_t DataType;
+    typedef std::vector<Eigen::Vector4f, Eigen::aligned_allocator<Eigen::Vector4f>> XYZYawVector;
+
+    /** Initialize a PoseGridHistory with the given x,y,z dimensions and x,y,z,yaw resolution. The
+     * grid cells are all initialized to 0.
+     */
+    PoseGridHistory(const Eigen::Vector3f& dimensions,
+                    const Eigen::Vector4f& resolution =
+                        Eigen::Vector4f(0.5f, 0.5f, 0.5f, 10.0f * se::math::deg_to_rad));
+
+    void record(const Eigen::Vector4f& pose);
+
+    void record(const Eigen::Matrix4f& pose);
+
+    DataType get(const Eigen::Vector4f& pose) const;
+
+    DataType get(const Eigen::Matrix4f& pose) const;
+
+    float rejectionProbability(const Eigen::Vector3f& position) const;
+
+    Eigen::Vector3f dimensions() const;
+
+    Eigen::Vector4f resolution() const;
+
+    Eigen::Vector4i dimensionsCells() const;
+
+    size_t size() const;
+
+    PoseVector neighbourPoses(const Eigen::Matrix4f& pose, const SensorImpl& sensor) const;
+
+    XYZYawVector visitedPoses() const;
+
+    bool writeGridMesh(const std::string& filename) const;
+
+    bool writeWedgeMesh(const std::string& filename) const;
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+
+
+    private:
+    const Eigen::Vector4f dim_;
+    const Eigen::Vector4f res_;
+    const Eigen::Vector4f inv_res_;
+    const Eigen::Vector4i size_;
+    const size_t num_cells_;
+    std::vector<DataType> grid_;
+
+    size_t indicesToLinearIndex(const Eigen::Vector4i& indices) const;
+
+    Eigen::Vector4i linearIndexToIndices(const size_t linear_idx) const;
+
+    Eigen::Vector4i poseToIndices(const Eigen::Vector4f& pose) const;
+
+    Eigen::Vector4f indicesToPose(const Eigen::Vector4i& pose) const;
+
+    size_t poseToIndex(const Eigen::Vector4f& pose) const;
+
+    Eigen::Vector4f indexToPose(const size_t idx) const;
+
+    static float getYaw(const Eigen::Matrix4f& pose);
+};
+
+} // namespace se
+
+#endif // POSE_GRID_HISTORY_HPP
---
+++ se_denseslam/test/pose_grid_history/pose_grid_history_unittest.cpp
@@ -0,0 +1,90 @@
+// SPDX-FileCopyrightText: 2021 Smart Robotics Lab, Imperial College London
+// SPDX-FileCopyrightText: 2021 Sotiris Papatheodorou
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <gtest/gtest.h>
+
+#include "se/pose_grid_history.hpp"
+
+
+
+class PoseGridTest : public ::testing::Test {
+    public:
+    const Eigen::Vector3f dimensions_;
+    const Eigen::Vector4f resolution_;
+    se::PoseGridHistory grid_;
+
+    PoseGridTest() :
+            dimensions_(2.0f, 2.0f, 1.0f),
+            resolution_(0.5f, 1.0f, 0.5f, 72.0f * se::math::deg_to_rad),
+            grid_(dimensions_, resolution_)
+    {
+    }
+};
+
+
+
+// Test that the grid was initialized to the expected number of cells.
+TEST_F(PoseGridTest, initialization)
+{
+    const Eigen::Vector4i s = grid_.dimensionsCells();
+    EXPECT_EQ(s.x(), 4);
+    EXPECT_EQ(s.y(), 2);
+    EXPECT_EQ(s.z(), 2);
+    EXPECT_EQ(s.w(), 5);
+    EXPECT_EQ(grid_.size(), 4 * 2 * 2 * 5);
+}
+
+
+
+TEST_F(PoseGridTest, increment)
+{
+    // Record various poses in the 0,0,0,0 cell.
+    const Eigen::Vector4f base_pose(0.0f, 0.0f, 0.0f, 0.0f);
+    EXPECT_EQ(grid_.get(base_pose), 0);
+    grid_.record(base_pose);
+    EXPECT_EQ(grid_.get(base_pose), 1);
+    Eigen::Matrix4f T = Eigen::Matrix4f::Identity();
+    T.topRightCorner<3, 1>() = grid_.resolution().head<3>() / 2.0f;
+    grid_.record(T);
+    EXPECT_EQ(grid_.get(base_pose), 2);
+    grid_.record(Eigen::Vector4f(0.0f, 0.0f, 0.0f, grid_.resolution().w() / 3.0f));
+    EXPECT_EQ(grid_.get(base_pose), 3);
+    grid_.record(Eigen::Vector4f(0.0f, 0.0f, 0.0f, -grid_.resolution().w() / 4.0f));
+    EXPECT_EQ(grid_.get(base_pose), 4);
+
+    // Record a pose in some other cell.
+    Eigen::Matrix4f pose4;
+    pose4 << 0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1;
+    EXPECT_EQ(grid_.get(pose4), 0);
+    grid_.record(pose4);
+    EXPECT_EQ(grid_.get(pose4), 1);
+}
+
+
+
+TEST_F(PoseGridTest, rejectionProbability)
+{
+    const Eigen::Vector4f pose(0.0f, 0.0f, 0.0f, 0.0f);
+    EXPECT_FLOAT_EQ(grid_.rejectionProbability(pose.head<3>()), 0.0f);
+    for (int i = 0; i < 2; i++) {
+        grid_.record(pose);
+        EXPECT_FLOAT_EQ(grid_.rejectionProbability(pose.head<3>()),
+                        1.0f / grid_.dimensionsCells().w());
+    }
+}
+
+
+
+TEST_F(PoseGridTest, visitedPoses)
+{
+    const Eigen::Vector4f pose(0.0f, 0.0f, 0.0f, 0.0f);
+    grid_.record(pose);
+
+    auto visited_poses = grid_.visitedPoses();
+    ASSERT_EQ(visited_poses.size(), 1u);
+    EXPECT_FLOAT_EQ(visited_poses.front().x(), pose.x());
+    EXPECT_FLOAT_EQ(visited_poses.front().y(), pose.y());
+    EXPECT_FLOAT_EQ(visited_poses.front().z(), pose.z());
+    EXPECT_FLOAT_EQ(visited_poses.front().w(), pose.w());
+}
