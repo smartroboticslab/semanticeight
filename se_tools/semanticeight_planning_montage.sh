851aa11fe00599af971ac8f8237ebf40429a4d67
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2022-04-12 09:39:33 +0100

    Update planning montage

--- se_tools/semanticeight_planning_montage.sh
+++ se_tools/semanticeight_planning_montage.sh
@@ -37,10 +37,11 @@ for depth in $depth_renders; do
 	entropy=$(printf '%s\n' "$depth" | sed 's/depth/entropy/' | null_if_not_file)
 	bg_gain=$(printf '%s\n' "$depth" | sed 's/depth/bg_gain/' | null_if_not_file)
 	object_gain=$(printf '%s\n' "$depth" | sed 's/depth/object_gain/' | null_if_not_file)
+	object_dist_gain=$(printf '%s\n' "$depth" | sed 's/depth/object_dist_gain/' | null_if_not_file)
 	out=$(printf '%s/%s\n' "$out_dir" "$(basename "$depth")" | sed 's/depth/render/')
 
-	printf 'montage -label %%t -font Liberation-Mono %s %s %s -geometry +2+2 -tile 1x %s\n' \
-		"$depth" "$entropy" "$bg_gain" "$out"
+	printf 'montage -label %%t -font Liberation-Mono %s %s %s %s %s -geometry +2+2 -tile 1x %s\n' \
+		"$depth" "$entropy" "$bg_gain" "$object_gain" "$object_dist_gain" "$out"
 done | parallel
 
 
