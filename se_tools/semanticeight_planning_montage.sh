d6ca9c360cef6bef513f99291d6c1f2ef8f9dabc
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2022-05-16 17:39:51 +0100

    Add object completion gain in planning montage

--- se_tools/semanticeight_planning_montage.sh
+++ se_tools/semanticeight_planning_montage.sh
@@ -38,10 +38,11 @@ for depth in $depth_renders; do
 	bg_gain=$(printf '%s\n' "$depth" | sed 's/depth/bg_gain/' | null_if_not_file)
 	object_gain=$(printf '%s\n' "$depth" | sed 's/depth/object_gain/' | null_if_not_file)
 	object_dist_gain=$(printf '%s\n' "$depth" | sed 's/depth/object_dist_gain/' | null_if_not_file)
+	object_compl_gain=$(printf '%s\n' "$depth" | sed 's/depth/object_compl_gain/' | null_if_not_file)
 	out=$(printf '%s/%s\n' "$out_dir" "$(basename "$depth")" | sed 's/depth/render/')
 
-	printf 'montage -label %%t -font Liberation-Mono %s %s %s %s %s -geometry +2+2 -tile 1x %s\n' \
-		"$depth" "$entropy" "$bg_gain" "$object_gain" "$object_dist_gain" "$out"
+	printf 'montage -label %%t -font Liberation-Mono %s %s %s %s %s %s -geometry +2+2 -tile 1x %s\n' \
+		"$depth" "$entropy" "$bg_gain" "$object_gain" "$object_dist_gain" "$object_compl_gain" "$out"
 done | parallel
 
 
