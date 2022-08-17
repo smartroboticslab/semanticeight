36b2e736c02da3bd5b038c2d423517aff81dd481
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2022-08-17 20:51:43 +0100

    Fix semanticeight_planning_montage.sh

--- se_tools/semanticeight_planning_montage.sh
+++ se_tools/semanticeight_planning_montage.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 set -eu
 
-usage() {
-	printf 'Usage: %s DIRECTORY\n' "${0##*/}"
-	printf '  Combine all images from each frame into one\n'
-	printf '  and place it in DIRECTORY_montage\n'
-}
-
-frame_number() {
-	printf '%s\n' "$1" | sed -E 's/^.*planning_([[:digit:]]{5})_.*\.png/\1/' | bc
-}
-
-null_if_not_file() {
-	f=$(cat)
-	if [ -f "$f" ]; then
+sibling_file() {
+	f=$(printf '%s\n' "$1" | sed 's/_depth\.png/_'"$2"'.png/')
+	if [ -f "$f" ]
+	then
 		printf '%s\n' "$f"
 	else
 		printf 'null:\n'
@@ -22,28 +13,26 @@ null_if_not_file() {
 
 
 
-if [ "$#" -ne 1 ]; then
-	usage
+if [ "$#" -ne 1 ]
+then
+	printf 'Usage: %s DIRECTORY\n' "${0##*/}"
+	printf '  Combine all images from each frame into one\n'
+	printf '  and place it in DIRECTORY_montage\n'
 	exit 2
 fi
 
-dir="${1%%/}"
-out_dir="$dir"_montage
+output_dir=$(printf '%s\n' "$1" | sed 's|/*$|_montage|')
+find "$1" -type f -name '*_depth.png' | while IFS= read -r depth
+do
+	entropy=$(sibling_file "$depth" 'entropy')
+	object_dist_gain=$(sibling_file "$depth" 'object_dist_gain')
+	bg_dist_gain=$(sibling_file "$depth" 'bg_dist_gain')
 
-depth_renders=$(find "$dir" -type f -name '*_depth.png' | sort -n)
-
-for depth in $depth_renders; do
-	entropy=$(printf '%s\n' "$depth" | sed 's/depth/entropy/' | null_if_not_file)
-	object_dist_gain=$(printf '%s\n' "$depth" | sed 's/depth/object_dist_gain/' | null_if_not_file)
-	bg_dist_gain=$(printf '%s\n' "$depth" | sed 's/depth/bg_dist_gain/' | null_if_not_file)
-
-	n=$(frame_number "$depth")
-	mkdir -p "$out_dir"
-	out=$(printf '%s/%s\n' "$out_dir" "$(basename "$depth")" | sed 's/depth/render/')
+	mkdir -p "$output_dir"
+	out=$(printf '%s/%s\n' "$output_dir" "${depth##*/}" |
+		sed 's/_depth\.png/_render.png/')
 
 	printf 'montage -label %%t -font Liberation-Mono '
 	printf '%s %s %s %s ' "$depth" "$entropy" "$object_dist_gain" "$bg_dist_gain"
-	printf '-geometry 320+2+2 -tile 1x %s\n' "$out"
+	printf ' -geometry 320x+2+2 -tile 1x %s\n' "$out"
 done | parallel
-
-
